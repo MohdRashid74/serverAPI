39a997fb33c92210c37eb7bbc5c5eb0bc3a36498
Rashid <profrashid74>
2023-07-15 07:21:26 +0000

    solution

---
+++ app.js
@@ -0,0 +1,22 @@
+let express = require("express");
+let { open } = require("sqlite");
+let sqlite3 = require("sqlite3");
+let path = require("path");
+let pathfile = path.join(__dirname, "moviesData.db");
+let app = express();
+app.use(express.json());
+let db = null;
+let initalizationdbserver = async () => {
+  try {
+    db = await open({
+      filename: pathfile,
+      server: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (error) {
+    console.log(`DB Error:${error.message}`);
+  }
+};
+initalizationdbserver();
